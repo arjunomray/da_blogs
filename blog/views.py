414bd8c57cf89a4b5e72893556f034f0cb8f2a01
arjunomray <arjunomray@gmail.com>
2024-01-22 12:02:10 +0530

    add comments functionality

--- blog/views.py
+++ blog/views.py
@@ -3,8 +3,9 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.urls import is_valid_path
 from django.core.mail import send_mail
 # from django.views.generic import ListView
-from .models import Post
-from .forms import EmailPostForm
+from .models import Post, Comment
+from .forms import EmailPostForm, CommentForm
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 
@@ -39,10 +40,15 @@ def post_detail(request, year, month, day, post):
                              publish__month=month,
                              publish__day=day,
                              )
+    comments = post.comments.filter(active=True)
+
+    form = CommentForm()
 
     return render(request,
                   'blog/post/detail.xhtml',
-                  {'post':post})
+                  {'post':post,
+                   'comments':comments,
+                   'form':form,})
 
 
 def post_share(request, post_id):
@@ -64,5 +70,25 @@ def post_share(request, post_id):
 
     form = EmailPostForm()
     return render(request, 'blog/post/share.xhtml', {'post':post, 'form':form, 'sent':sent})
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post,
+                             id=post_id,
+                             status=Post.Status.PUBLISHED)
+    comment = None
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        
+        comment.post = post
+
+        comment.save()
+    return render(request,'blog/post/comment.xhtml',{
+        'post':post,
+        'form':form,
+        'comment':comment
+        })
 
 
