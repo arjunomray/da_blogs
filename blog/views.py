05ae7c131ebbb287756265d58e560b07031222be
arjunomray <arjunomray@gmail.com>
2024-01-25 12:56:07 +0530

    add search functionality

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.urls import is_valid_path
-from django.core.mail import send_mail
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from taggit.models import Tag
 # from django.views.generic import ListView
 from .models import Post, Comment
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from django.views.decorators.http import require_POST
 
 # Create your views here.
@@ -107,4 +107,24 @@ def post_comment(request, post_id):
         'comment':comment
         })
 
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', weight='A')+SearchVector('body', weight='B')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query),
+            ).filter(rand__gte=0.3).order_by('-rank')
+    return render(request, 'blog/post/search.xhtml',{
+        'form':form,
+        'results':results,
+        'query':query,
+    })
 
